719a64a9af328d3019c01ea4b2bf33a22a4d0d0d
DESKTOP-H32MV99\dokap <dokaponkit@gmail.com>
2023-08-03 22:32:20 +0700

    session user

--- src/app/components/user.component.tsx
+++ src/app/components/user.component.tsx
@@ -10,10 +10,10 @@ export const User = () => {
       <h1>Client Session</h1>
       <div>
         <ul>
-          <li>Name : {session?.user.firstname}</li>
-          <li>Lastname :{session?.user.lastname}</li>
+          <li>Name : {session?.user?.firstname}</li>
+          <li>Lastname :{session?.user?.lastname}</li>
           <li>Email : {session?.user?.email}</li>
-          <li>School ID :{session?.user.school}</li>
+          <li>School ID :{session?.user?.school}</li>
         </ul>
       </div>
     </>
